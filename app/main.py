905fd3e48786ebb7a072db79d0676df13cda889b
KauaVictor077 <kaua38460@gmail.com>
2026-03-01 19:55:11 -0300

    Enhance Animal class with new attributes and methods
    Add name, health, and hidden attributes to Animal class

--- app/main.py
+++ app/main.py
@@ -3,3 +3,23 @@ class Animal:
 
     def __init__(
             self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False) -> None:
+        self.name = name
+        self._health = health
+        self.hidden = hidden
+
+class Herbivore(Animal):
+
+    def hide(self: Animal) -> None: 
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+
+    def bite(
+            self,
+            target: Animal) -> None:
+        if isinstance(target, Herbivore) and not target.hidden:
+            target.health -= 50
