c66bd4e4e0d46a73a579c8525e69c4bcd2222c8e
KauaVictor077 <kaua38460@gmail.com>
2026-03-01 20:12:58 -0300

    Refactor Animal class and add health property

--- app/main.py
+++ app/main.py
@@ -1,26 +1,30 @@
 class Animal:
-    alive: list["Animal"] = []
+    alive = []
 
     def __init__(
-            self,
-            name: str,
-            health: int = 100,
-            hidden: bool = False) -> None:
+        self,
+        name: str,
+        health: int = 100,
+        hidden: bool = False
+    ) -> None:
         self.name = name
         self._health = health
         self.hidden = hidden
+        Animal.alive.append(self)
 
+    @property
+    def health(self) -> int:
+        return self._health
 
-class Herbivore(Animal):
+    @health.setter
+    def health(self, value: int) -> None:
+        self._health = value
+        if self._health <= 0 and self in Animal.alive:
+            Animal.alive.remove(self)
 
-    def hide(self: Animal) -> None:
-        self.hidden = not self.hidden
-
-
-class Carnivore(Animal):
-
-    def bite(
-            self,
-            target: Animal) -> None:
-        if isinstance(target, Herbivore) and not target.hidden:
-            target.health -= 50
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
